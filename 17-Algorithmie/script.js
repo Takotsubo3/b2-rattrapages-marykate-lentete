d3c3a4ccf5c90c4dfd813d45096d2188717fd8b6
Celena L'entete <claire.lentete@outlook.com>
2026-06-16 21:54:41 +0400

    feat: create folder and ingredients and base lists

---
+++ 17-Algorithmie/script.js
@@ -0,0 +1,24 @@
+const bases = [
+  { name: 'Épinard', category: 'Salade', price: 2.5, available: true },
+  { name: 'Iceberg', category: 'Salade', price: 2.0, available: false },
+  { name: 'Mâche', category: 'Salade', price: 2.8, available: true },
+  { name: 'Pâtes', category: 'Féculent', price: 3.0, available: true },
+  {
+    name: 'Pâtes complètes',
+    category: 'Féculent',
+    price: 3.2,
+    available: true,
+  },
+  { name: 'Quinoa', category: 'Féculent', price: 3.8, available: true },
+];
+
+const ingredients = [
+  { name: 'Betteraves', category: 'Légume', price: 1.5, available: false },
+  { name: 'Carottes', category: 'Légume', price: 1.2, available: true },
+  { name: 'Emmental', category: 'Fromage', price: 2.5, available: true },
+  { name: 'Mimolette', category: 'Fromage', price: 2.8, available: false },
+  { name: 'Oeuf', category: 'Protéine', price: 1.5, available: false },
+  { name: 'Poulet', category: 'Protéine', price: 2.0, available: true },
+  { name: 'Tomates cerises', category: 'Légume', price: 2.3, available: true },
+  { name: 'Avocat', category: 'Fruit', price: 3.0, available: false },
+];
